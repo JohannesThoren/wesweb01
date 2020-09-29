77ca34ead70f549982c0dfd2551548235c1569e4
Johannes Thorén <45944438+JohannesThoren@users.noreply.github.com>
2020-09-29 12:04:03 +0200

    poof
    poof

---
+++ uppgift7/main.js
@@ -0,0 +1,57 @@
+/*
+ *   Copyright (c) 2020 Johannes Thorén
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+function eight_ball(num) {
+    switch(num) {
+        case 1:
+            document.write("Esse laborum nostrud et consequat excepteur proident esse aliquip exercitation mollit reprehenderit labore.");
+            break
+        case 2:
+            document.write("Id ut aute et velit ut adipisicing incididunt magna ut sit sunt.");
+            break
+        case 3:
+            document.write("Laboris proident aliquip sit tempor ea voluptate incididunt voluptate sint ex amet voluptate dolore mollit.");
+            break
+        case 4:
+            document.write("Eu nostrud anim ullamco quis reprehenderit ea duis ut Lorem eu dolor in deserunt labore.");
+            break
+        case 5:
+            document.write("Anim amet exercitation deserunt non nisi.");
+            break
+        case 6:
+            document.write("Reprehenderit minim sit qui consectetur nulla.")
+            break
+        case 7:
+            document.write("Laborum non cillum elit mollit est magna esse non ad ut ipsum.")
+            break
+        case 8:
+            document.write("Dolore adipisicing incididunt commodo eu.")
+            break
+        case 9:
+            document.write("Officia occaecat eiusmod non aliquip dolor sit tempor.")
+            break
+        case 10:
+            document.write("Ut esse proident ullamco aliquip voluptate dolor enim do sit magna.")
+            break
+    }
+}
