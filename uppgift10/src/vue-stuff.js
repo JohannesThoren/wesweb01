3cacdff12975d5676084fc1569e13050724becaa
Johannes Thorén <45944438+JohannesThoren@users.noreply.github.com>
2020-10-07 08:32:48 +0200

    poof

---
+++ uppgift10/src/vue-stuff.js
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (c) 2020 Johannes Thorén
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+var btn_load_event = new Vue({
+    el: '#btn_load',
+    methods: {
+        fn_load: function (event) {
+            if (event) {
+                console.log(data._data.gist_id)
+            }
+            else {
+                alert("something went wrong!")
+            }
+        }
+    }
+})
+
+var btn_save_event = new Vue({
+    el: '#btn_save',
+    methods: {
+        fn_save: function (event) {
+            if (event) {
+
+            }
+            else {
+                alert("something went wrong!")
+            }
+        }
+    }
+})
+
+var btn_menu_event = new Vue({
+    el: '#btn_menu',
+    methods: {
+        fn_menu: function (event) {
+            if (event) {
+
+            }
+            else {
+                alert("something went wrong!")
+            }
+        }
+    }
+})
