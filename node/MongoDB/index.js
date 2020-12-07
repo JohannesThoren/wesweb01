87e2d40d17c345ccdf67899c35b637ed2efe88d6
Johannes Thorén <Johannes.Thoren@outlook.com>
2020-12-07 16:09:14 +0100

    Poof!

--- node/MongoDB/index.js
+++ node/MongoDB/index.js
@@ -1,49 +1,39 @@
-/*
- *   Copyright (c) 2020 Johannes Thorén
- *   All rights reserved.
+const exp = require("express");
+const app = exp();
+const bp = require("body-parser");
+const moon = require("mongoose");
 
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- 
- *   The above copyright notice and this permission notice shall be included in all
- *   copies or substantial portions of the Software.
- 
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *   SOFTWARE.
- */
+app.set("view engine", "ejs");
+app.use(bp.urlencoded({extended: true}));
+app.use(exp.static("resources"));
 
-const exp = require('express')
-const app = exp()
-const bp = require('body-parser')
-const mg = require('mongoose')
+moon.connect("mongodb://localhost:27017/Users", {useNewUrlParser: true, useUnifiedTopology:true})
 
-mg.connect("mongodb://localhost:27017/test")
-
-const TestSchema = new mg.Schema({ 
-    test: String
+const UsrSchema = new moon.Schema({
+    anvNamn: String,
+    pass: String,
+    fNamn: String,
+    eNamn:String,
+    age: Number,
+    school: Boolean
 });
 
-let Test = new mg.model("user", TestSchema);
+let User = moon.model("User", UsrSchema);
 
-Test.create({
-    test: "test"
+/* User.create({
+    anvNamn: "Niklas",
+    pass: "happy",
+    fNamn: "Niklas",
+    eNamn: "Oscarsson",
+    age: 34,
+    school: false
+}); */
 
-})
-
-app.listen(3000, (err) => {
-    if(err) {
+app.listen(3000, function(err){
+    if(err){
         console.log(err);
-    }
-    else {
-        console.log("connected");
+        console.log("någonting blev fel");
+    }else{
+        console.log("Connected");
     }
 })
