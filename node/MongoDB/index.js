56864b4dcb5bb8d5ea6c46199072ca489752338f
Johannes Thoren <johannes.thoren@outlook.com>
2020-12-09 09:27:28 +0100

    Poof!

--- node/MongoDB/index.js
+++ node/MongoDB/index.js
@@ -2,31 +2,47 @@ const exp = require("express");
 const app = exp();
 const bp = require("body-parser");
 const moon = require("mongoose");
+const mo = require("method-override"); 
+
 
 app.set("view engine", "ejs");
-app.use(bp.urlencoded({extended: true}));
+app.use(bp.urlencoded({ extended: true }));
 app.use(exp.static("resources"));
+app.use(mo("_method"));
 
-moon.connect("mongodb://localhost:27017/Users", {useNewUrlParser: true, useUnifiedTopology:true})
+moon.connect("mongodb://localhost:27017/Users", { useNewUrlParser: true, useUnifiedTopology: true })
 
 const UsrSchema = new moon.Schema({
     usr: String,
     pass: String,
     fNamn: String,
-    eNamn:String,
+    eNamn: String,
     age: Number,
     school: Boolean
 });
 
 let User = moon.model("User", UsrSchema);
 
-app.get("/" ,(req, res)=> {
-    res.render("home");
+app.get("/", (req, res) => {
+    res.redirect("/index")
 });
 
-app.post("/" ,(req, res)=> {
+app.get("/index", (req, res) => {
+    User.find({}, (err, data)=> {
+        if(err) {
+            res.send("404 - site not found")
+
+        } else {
+            console.log(data)
+            let dat = data;
+            res.render("index", {data: dat})
+        }
+    })
+});
+
+app.post("/", (req, res) => {
     console.log(req.body);
-    
+
     let usr = req.body.usr;
     let pass = req.body.pass;
     let fNamn = req.body.fNamn;
@@ -43,14 +59,14 @@ app.post("/" ,(req, res)=> {
         school: inSchool
     });
 
-    // res.redirect("/")
+    res.redirect("/");
 });
 
-app.listen(8000, function(err){
-    if(err){
+app.listen(8000, function (err) {
+    if (err) {
         console.log(err);
         console.log("någonting blev fel");
-    }else{
+    } else {
         console.log("Connected");
     }
 })
